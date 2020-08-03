431d7b6d347c987f7a18965a4f688aaa230cb0e6
Sorrell Sohota <sorrellsohota@Macbooks-MBP.hitronhub.home>
2020-08-03 12:58:24 -0700

    uploaded firebase, finished progress bar useeffect

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -7,7 +7,7 @@ const UploadForm = () => {
 
   const types = ["image/png", "image/jpeg"];
 
-  const changeHandler = (e) => {
+  const handleChange = (e) => {
     let selected = e.target.files[0];
 
     if (selected && types.includes(selected.type)) {
@@ -15,16 +15,19 @@ const UploadForm = () => {
       setError("");
     } else {
       setFile(null);
-      setError("please select an image file (png or jpeg)");
+      setError("Please select an image file (png or jpg)");
     }
   };
+
   return (
     <form>
-      <input type="file" onChange={changeHandler} />
+      <label>
+        <input type="file" onChange={handleChange} />
+      </label>
       <div className="output">
-        {error && <div className="error"> {error} </div>}
-        {file && <div> {file.name}</div>}
-        {file && <ProgressBar file={file} setfile={setFile} />}
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
       </div>
     </form>
   );
